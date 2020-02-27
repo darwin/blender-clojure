3314d2c94eae978e48d50bb94a1cc4b4a6b3fb87
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-27 14:03:52 +0100

    scripts: refactor install-dependencies a bit, use pip install --upgrade

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,6 +4,8 @@ set -e -o pipefail
 # shellcheck source=_config.sh
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 
+KNOWN_PYTHON_DEPS=(hy rply astor clint colorama funcparserlib)
+
 cd "$ROOT_DIR"
 
 set -x
@@ -12,4 +14,4 @@ cd "$HYLC_BLENDER_PYTHON_PATH"
 # this worked for me on macOS 10.15.4, blender 2.82
 bin/python3.7m -m ensurepip
 bin/python3.7m -m pip install -U pip
-bin/pip3 install hy rply astor clint colorama funcparserlib -t "$WATCHER_DIR/modules"
+bin/pip3 install --upgrade "${KNOWN_PYTHON_DEPS[@]}" -t "$WATCHER_DIR/modules"
