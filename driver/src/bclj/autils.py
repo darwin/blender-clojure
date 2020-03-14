cca046092df1162ba0d518a6364c2361080ad65b
Antonin Hildebrand <antonin@hildebrand.cz>
2020-03-14 20:20:07 +0100

    driver: introduce autils

---
+++ driver/src/bclj/autils.py
@@ -0,0 +1,28 @@
+import inspect
+import logging
+import asyncio
+from functools import wraps
+
+logger = logging.getLogger('bclj.autils')
+
+
+def wrap_coroutine_with_exceptions_reporting(coro, *args, **kwargs):
+    @wraps(coro)
+    async def coroutine_wrapper():
+        try:
+            return await coro(*args, **kwargs)
+        except Exception:
+            logger.exception("Unhandled exception in async call to {} with args={} kwargs={}".format(coro, args, kwargs),
+                             stack_info=True)
+
+    return coroutine_wrapper
+
+
+def call_soon(loop, coro, *args, **kwargs):
+    assert inspect.iscoroutinefunction(coro)
+
+    def callback():
+        wrapped_coro = wrap_coroutine_with_exceptions_reporting(coro, *args, **kwargs)
+        asyncio.ensure_future(wrapped_coro(), loop=loop)
+
+    loop.call_soon_threadsafe(callback)
