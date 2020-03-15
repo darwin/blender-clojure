a008575bbac1b170d35747aa5410f5847b97031c
Antonin Hildebrand <antonin@hildebrand.cz>
2020-03-15 16:55:54 +0100

    driver: extract blender-related code into blender.py

---
+++ driver/src/bclj/blender.py
@@ -0,0 +1,50 @@
+import logging
+import bpy  # import blender
+
+from bclj import jobs, worker, hy
+
+logger = logging.getLogger(__name__)
+
+
+# noinspection PyMethodMayBeStatic
+class ModalTimerOperator(bpy.types.Operator):
+    """Operator which runs its self from a timer, we use it to implement our event loop"""
+    bl_idname = "wm.modal_timer_operator"
+    bl_label = "BCLJ Event Loop Operator"
+
+    _timer = None
+
+    def modal(self, _context, event):
+        if event.type == 'TIMER':
+            worker.drain_asyncio_event_loop()
+            jobs.process_pending_session_jobs()
+            hy.check_live_file()
+        return {'PASS_THROUGH'}
+
+    def execute(self, context):
+        wm = context.window_manager
+        self._timer = wm.event_timer_add(0.1, window=context.window)
+        wm.modal_handler_add(self)
+        hy.log_live_file_watching_start()
+        return {'RUNNING_MODAL'}
+
+    def cancel(self, context):
+        wm = context.window_manager
+        wm.event_timer_remove(self._timer)
+        hy.log_live_file_watching_stop()
+
+
+def frame_change_handler(_scene):
+    hy.run_live_file()
+
+
+def register():
+    bpy.utils.register_class(ModalTimerOperator)
+    bpy.app.handlers.frame_change_post.append(frame_change_handler)
+    # this is important to instantiate our operator and kick off the timer
+    bpy.ops.wm.modal_timer_operator()
+
+
+def unregister():
+    bpy.utils.unregister_class(ModalTimerOperator)
+    bpy.app.handlers.frame_change_post.remove(frame_change_handler)
--- driver/src/bclj/main.py
+++ driver/src/bclj/main.py
@@ -1,54 +1,7 @@
-import os
-
 import logging
-from bclj import jobs, env_info, log, worker, js
-
-import bpy  # import blender
+from bclj import env_info, js, hy, blender
 
 logger = logging.getLogger(__name__)
-
-from bclj import hy
-
-
-class ModalTimerOperator(bpy.types.Operator):
-    """Operator which runs its self from a timer"""
-    bl_idname = "wm.modal_timer_operator"
-    bl_label = "Modal Timer Operator"
-
-    _timer = None
-
-    def modal(self, _context, event):
-        if event.type == 'TIMER':
-            worker.drain_asyncio_event_loop()
-            jobs.process_pending_session_jobs()
-            hy.check_live_file()
-        return {'PASS_THROUGH'}
-
-    def execute(self, context):
-        wm = context.window_manager
-        self._timer = wm.event_timer_add(0.1, window=context.window)
-        wm.modal_handler_add(self)
-        hy.log_live_file_watching_start()
-        return {'RUNNING_MODAL'}
-
-    def cancel(self, context):
-        wm = context.window_manager
-        wm.event_timer_remove(self._timer)
-        hy.log_live_file_watching_stop()
-
-
-def frame_change_handler(_scene):
-    hy.run_live_file()
-
-
-def register():
-    bpy.utils.register_class(ModalTimerOperator)
-    bpy.app.handlers.frame_change_post.append(frame_change_handler)
-
-
-def unregister():
-    bpy.utils.unregister_class(ModalTimerOperator)
-    bpy.app.handlers.frame_change_post.remove(frame_change_handler)
 
 
 def print_welcome():
@@ -57,8 +10,6 @@ def print_welcome():
 
 def start():
     print_welcome()
-    hy.start_hyrepl()
-    register()
-    # test call
-    bpy.ops.wm.modal_timer_operator()
     js.bootstrap()
+    hy.start_hyrepl()
+    blender.register()
