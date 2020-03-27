072e8eb31a0dd160edaae986b7e828f1e95aaf46
Antonin Hildebrand <antonin@hildebrand.cz>
2020-03-27 13:33:53 +0100

    bcljs: minor tweaks

--- bcljs/src/lib/bcljs/runtime.cljs
+++ bcljs/src/lib/bcljs/runtime.cljs
@@ -1,6 +1,6 @@
 (ns bcljs.runtime
   (:require [bcljs.runtime.marshalling :as marshalling]))
 
-; calls to these functions can be emitted by compiler macros
+; calls to these functions may be emitted by compiler macros
 
 (def marshal-kw-args marshalling/marshal-kw-args)
